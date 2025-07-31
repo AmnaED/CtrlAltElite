2117a7801a30a72616dff83b25702593c06f87a5
ranyaelshinawy <ranyaelshinawy@gmail.com>
2025-07-30 22:38:29 -0500

    Connected MongoDB, imported databases/collections and hid the MongoDB password in .env file

--- server/app.py
+++ server/app.py
@@ -1,9 +1,29 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
 from encryption import encrypt, decrypt
+from dotenv import load_dotenv
+from pymongo import MongoClient
+import os
+
+# Load environment variables from .env
+load_dotenv()
 
 app = Flask(__name__)
 CORS(app)
+
+# Get MongoDB password and connect to database
+mongo_pass = os.getenv("MONGO_PASSWORD")
+link = f"mongodb+srv://ranyae:{mongo_pass}@apad-project.qvgsgr3.mongodb.net/?retryWrites=true&w=majority"
+client = MongoClient(link)
+
+print(mongo_pass)
+# Accessing tables
+resource_db = client["resource-management-db"]
+resources_collection = resource_db["resources"]
+project_db = client["project-table-db"]
+project_collection = project_db["project-table"]
+user_db = client["user-management-db"]
+user_collection = project_db["user-management"]
 
 @app.route("/")
 def home():
